7995629201cb14be52afcd87620159fd4156114c
Bos Eriko Reyes <bos.eriko@gmail.com>
2019-11-16 13:42:12 +0800

    [Finishes #3] Work on Footer v1
    # Story Title
    
    [Work on Footer v1](https://github.com/BosEriko/boseriko.com/issues/3)
    
    # Changes made
    
    - Make a component for footer
    - Add stuff on footer
    
    # How does the solution address the problem
    
    This PR will add a footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="tracking-wider min-h-screen text-xl subpixel-antialiased">
-    <nuxt />
+    <div class="flex flex-col min-h-screen">
+      <Header />
+      <div class="flex-1"><nuxt /></div>
+      <Footer />
+    </div>
   </div>
 </template>
 
 <style>
 @import url('https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300&display=swap');
+
 body {
   font-family: 'Open Sans Condensed', sans-serif;
 }
+
+.smooth {
+  transition: all 0.25s ease-in-out;
+}
 </style>
+
+<script>
+import Header from '~/components/Header.vue'
+import Footer from '~/components/Footer.vue'
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
