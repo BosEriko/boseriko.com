cfa20dea138391e77207b0d6d1c6511b3f1c7090
Bos Eriko Reyes <bos.eriko@gmail.com>
2019-11-16 13:27:21 +0800

    [Finishes #2] Work on Header v1
    # Story Title
    
    [Work on Header v1](https://github.com/BosEriko/boseriko.com/issues/2)
    
    # Changes made
    
    - Clean boilerplate
    - Add menu
    - Add cute doggo
    
    # How does the solution address the problem
    
    This PR will add a Header

---
+++ components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+  <header class="bg-white border-b">
+    <div class="flex items-center mx-auto container">
+      <div v-if="!isMenuActive" class="mr-auto">
+        <img class="h-10" src="/img/logo.png" alt="Bos Eriko" />
+      </div>
+      <ul v-if="isMenuActive" class="mr-auto flex">
+        <li class="mr-5">
+          <a class="hover:underline" href="/Github">GitHub</a>
+        </li>
+        <li class="mr-5">
+          <a class="hover:underline" href="/Twitch">Twitch</a>
+        </li>
+        <li>
+          <a class="hover:underline" href="/Twitter">Twitter</a>
+        </li>
+      </ul>
+      <button
+        @click="toggleMenu"
+        :class="
+          `
+            hamburger
+            hamburger--slider
+            ${isMenuActive ? 'is-active' : ''}
+          `
+        "
+        type="button"
+      >
+        <span class="hamburger-box">
+          <span class="hamburger-inner"></span>
+        </span>
+      </button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      isMenuActive: false
+    }
+  },
+  methods: {
+    // eslint-disable-next-line
+    toggleMenu: function () {
+      this.isMenuActive = !this.isMenuActive
+    }
+  }
+}
+</script>
+
+<style scoped>
+button:focus {
+  outline: none;
+}
+.hamburger {
+  transform: scale(0.5, 0.5);
+}
+</style>
