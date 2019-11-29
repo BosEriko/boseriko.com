0f72eeca9aede1e203830660a0c5383aad7d27c1
BosEriko <bos.eriko@gmail.com>
2019-11-29 13:41:46 +0800

    add gatsbyjs to footer

--- src/components/Footer/component.js
+++ src/components/Footer/component.js
@@ -3,7 +3,7 @@ import React from "react"
 
 const Footer = ({ siteTitle }) => (
   <footer className="bg-white text-center p-5">
-    Made with <i className="fas fa-heart"></i> by {siteTitle} &copy; {new Date().getFullYear()}
+    Made with <i className="fas fa-heart"></i> by {siteTitle} using <a href="https://www.gatsbyjs.org/" target="_blank" rel="noopener noreferrer" className="hover:no-underline underline">GatsbyJS</a> &copy; {new Date().getFullYear()}
   </footer>
 )
 
