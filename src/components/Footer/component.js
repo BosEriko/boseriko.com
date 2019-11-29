20095f75a870b7f957b86736891dc116698d6ec2
BosEriko <bos.eriko@gmail.com>
2019-11-29 11:54:57 +0800

    add links on the upper right

--- src/components/Footer/component.js
+++ src/components/Footer/component.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Footer = ({ siteTitle }) => (
-  <footer className="bg-black text-white">
+  <footer className="bg-white text-center py-5">
     {siteTitle} &copy; {new Date().getFullYear()}
   </footer>
 )
