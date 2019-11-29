2db71740466a2a975aec9f51e9edbfd3deed4663
BosEriko <bos.eriko@gmail.com>
2019-11-29 12:13:36 +0800

    add url to links

--- src/components/Header/component.js
+++ src/components/Header/component.js
@@ -1,23 +1,25 @@
 import PropTypes from "prop-types"
 import React from "react"
+import ReactTooltip from 'react-tooltip'
 
 const Header = ({ siteTitle }) => (
   <header className="bg-white">
+    <ReactTooltip />
     <div className="container mx-auto flex">
       <h1 className="font-bebas m-0 p-0 text-5xl">BE</h1>
       <ul className="flex ml-auto items-center text-2xl">
         <li className="mr-5">
-          <a href="#Link" className="hover:opacity-50 smooth" style={{ color: "#1ba1f2" }}>
+          <a href="http://twitter.boseriko.com/" className="hover:opacity-50 smooth" style={{ color: "#1ba1f2" }} data-tip="Twitter" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-twitter"></i>
           </a>
         </li>
         <li className="mr-5">
-          <a href="#Link" className="color-black hover:opacity-50 smooth">
+          <a href="https://dev.to/boseriko" className="color-black hover:opacity-50 smooth" data-tip="DEV Community" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-dev"></i>
           </a>
         </li>
         <li>
-          <a href="#Link" className=" hover:opacity-50 smooth" style={{ color: "#9147ff" }}>
+          <a href="http://stream.boseriko.com/" className=" hover:opacity-50 smooth" style={{ color: "#9147ff" }} data-tip="Twitch" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-twitch"></i>
           </a>
         </li>
