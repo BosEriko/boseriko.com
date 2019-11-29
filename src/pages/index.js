f368181c5dea80838bbc7b883482513d6c3967f1
BosEriko <bos.eriko@gmail.com>
2019-11-29 13:01:55 +0800

    add welcome message

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,8 +7,10 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className="container mx-auto px-5">
-      <div>Under Maintenance</div>
-      <Link to="/page-2/">Go to page 2</Link>
+      <div className="text-center font-bebas">
+        <h2 className="text-6xl">Hello! My name is Bos Eriko</h2>
+        <h3 className="text-gray-500 text-3xl">UI/UX Designer — Fullstack Website Developer using Javascript and Ruby</h3>
+      </div>
     </div>
   </Layout>
 )
