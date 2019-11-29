1b15587e10b435b23a7fb389634fdd4074aabfc1
BosEriko <bos.eriko@gmail.com>
2019-11-29 18:03:33 +0800

    remove gatsbyjs on footer

--- src/components/Footer/component.js
+++ src/components/Footer/component.js
@@ -3,7 +3,7 @@ import React from "react"
 
 const Footer = ({ siteTitle }) => (
   <footer className="bg-white text-center p-5">
-    Made with <i className="fas fa-heartbeat"></i> by {siteTitle} using <a href="https://www.gatsbyjs.org/" target="_blank" rel="noopener noreferrer" className="hover:no-underline underline">GatsbyJS</a> <i className="far fa-copyright"></i> {new Date().getFullYear()}
+    Made with <i className="fas fa-heartbeat"></i> by {siteTitle} <i className="far fa-copyright"></i> {new Date().getFullYear()}
   </footer>
 )
 
