c76ab1029eeaf83a00be5370a8c14a28279bc0a5
BosEriko <bos.eriko@gmail.com>
2019-11-29 16:00:36 +0800

    change mr-5 to mr-8

--- src/components/Header/component.js
+++ src/components/Header/component.js
@@ -6,22 +6,22 @@ const Header = ({ siteTitle }) => (
     <div className="container mx-auto flex px-5">
       <h1 className="font-bebas m-0 p-0 text-5xl">BE</h1>
       <ul className="flex ml-auto items-center text-2xl">
-        <li className="mr-5">
+        <li className="mr-8">
           <a href="http://github.boseriko.com/" className="hover:opacity-50 smooth" data-tip="GitHub" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-github"></i>
           </a>
         </li>
-        <li className="mr-5">
+        <li className="mr-8">
           <a href="http://twitter.boseriko.com/" className="hover:opacity-50 smooth" data-tip="Twitter" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-twitter"></i>
           </a>
         </li>
-        <li className="mr-5">
+        <li className="mr-8">
           <a href="https://dev.to/boseriko" className="hover:opacity-50 smooth" data-tip="DEV Community" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-dev"></i>
           </a>
         </li>
-        <li className="mr-5">
+        <li className="mr-8">
           <a href="http://stream.boseriko.com/" className="hover:opacity-50 smooth" data-tip="Twitch" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-twitch"></i>
           </a>
