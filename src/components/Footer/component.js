d89d5b08e5afb25dbc3fa403d8647084ba17ae2c
BosEriko <bos.eriko@gmail.com>
2019-11-29 13:03:43 +0800

    update footer

--- src/components/Footer/component.js
+++ src/components/Footer/component.js
@@ -3,7 +3,7 @@ import React from "react"
 
 const Footer = ({ siteTitle }) => (
   <footer className="bg-white text-center p-5">
-    {siteTitle} &copy; {new Date().getFullYear()}
+    Made with <span className="text-red-400"><i className="fas fa-heart"></i></span> by {siteTitle} &copy; {new Date().getFullYear()}
   </footer>
 )
 
