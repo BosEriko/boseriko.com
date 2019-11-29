e90616923cc939e679c935270cd36de422f28142
BosEriko <bos.eriko@gmail.com>
2019-11-29 13:39:41 +0800

    remoev colors

--- src/components/Footer/component.js
+++ src/components/Footer/component.js
@@ -3,7 +3,7 @@ import React from "react"
 
 const Footer = ({ siteTitle }) => (
   <footer className="bg-white text-center p-5">
-    Made with <span className="text-red-400"><i className="fas fa-heart"></i></span> by {siteTitle} &copy; {new Date().getFullYear()}
+    Made with <i className="fas fa-heart"></i> by {siteTitle} &copy; {new Date().getFullYear()}
   </footer>
 )
 
--- src/components/Header/component.js
+++ src/components/Header/component.js
@@ -7,27 +7,27 @@ const Header = ({ siteTitle }) => (
       <h1 className="font-bebas m-0 p-0 text-5xl">BE</h1>
       <ul className="flex ml-auto items-center text-2xl">
         <li className="mr-5">
-          <a href="http://github.boseriko.com/" className="color-gray-800 hover:opacity-50 smooth" data-tip="GitHub" target="_blank" rel="noopener noreferrer">
+          <a href="http://github.boseriko.com/" className="hover:opacity-50 smooth" data-tip="GitHub" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-github"></i>
           </a>
         </li>
         <li className="mr-5">
-          <a href="http://twitter.boseriko.com/" className="hover:opacity-50 smooth" style={{ color: "#1ba1f2" }} data-tip="Twitter" target="_blank" rel="noopener noreferrer">
+          <a href="http://twitter.boseriko.com/" className="hover:opacity-50 smooth" data-tip="Twitter" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-twitter"></i>
           </a>
         </li>
         <li className="mr-5">
-          <a href="https://dev.to/boseriko" className="color-black hover:opacity-50 smooth" data-tip="DEV Community" target="_blank" rel="noopener noreferrer">
+          <a href="https://dev.to/boseriko" className="hover:opacity-50 smooth" data-tip="DEV Community" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-dev"></i>
           </a>
         </li>
         <li className="mr-5">
-          <a href="http://stream.boseriko.com/" className="hover:opacity-50 smooth" style={{ color: "#9147ff" }} data-tip="Twitch" target="_blank" rel="noopener noreferrer">
+          <a href="http://stream.boseriko.com/" className="hover:opacity-50 smooth" data-tip="Twitch" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-twitch"></i>
           </a>
         </li>
         <li>
-          <a href="http://resume.boseriko.com/" className="text-teal-500 hover:opacity-50 smooth" data-tip="Resume" target="_blank" rel="noopener noreferrer">
+          <a href="http://resume.boseriko.com/" className="hover:opacity-50 smooth" data-tip="Resume" target="_blank" rel="noopener noreferrer">
             <i className="fas fa-file-alt"></i>
           </a>
         </li>
