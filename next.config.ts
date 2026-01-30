fe72ea19bc493b2c7d455efdef125cb2a9c6f196
BosEriko <bos@eriko.ph>
2026-01-30 18:11:34 +0800

    Feature: Move Deploy Script to Verce

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/boseriko.com",
-  assetPrefix: "/boseriko.com/",
+  /* config options here */
 };
 
 export default nextConfig;
