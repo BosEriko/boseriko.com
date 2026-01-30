942ecc88dc84c6dbdda5243cabbf264130f74fb3
BosEriko <bos@eriko.ph>
2026-01-30 16:50:22 +0800

    Feature: Add Deploy Script

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/boseriko.com",
+  assetPrefix: "/boseriko.com/",
 };
 
 export default nextConfig;
