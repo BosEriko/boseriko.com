891379fc6ba54ff10c5db1067ef3810f49c3f498
BosEriko <bos@eriko.ph>
2026-02-02 11:46:14 +0800

    Hotfix: Disable NODE_TLS_REJECT on local

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
+if (process.env.NODE_ENV === "development") {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
