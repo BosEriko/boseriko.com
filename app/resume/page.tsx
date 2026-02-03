f77e911016541d539bc1b5fe1d7e343923748580
BosEriko <bos@eriko.ph>
2026-02-03 19:22:44 +0800

    Feature: Add Font Awesome and Links to Resume

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,4 +1,8 @@
 import Template from "@template";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 import experience from "../../data/experience.json";
 
 export default function Resume() {
@@ -7,9 +11,23 @@ export default function Resume() {
       {/* Header */}
       <div className="text-left mb-5">
         <h1 className="text-4xl font-bold mb-2">Bos Eriko Reyes</h1>
-        <p className="text-gray-600">
+        <p className="text-gray-600 mb-5">
           Full Stack Developer & Software Engineer
         </p>
+        <div className="flex items-center text-sm text-gray-700">
+          <div className="flex items-center gap-2 flex-1">
+            <FontAwesomeIcon icon={faGlobe} className="h-4 w-4" />
+            <span>www.boseriko.com</span>
+          </div>
+          <div className="flex items-center gap-2 flex-1">
+            <FontAwesomeIcon icon={faGithub} className="h-4 w-4" />
+            <span>github.boseriko.com</span>
+          </div>
+          <div className="flex items-center gap-2 flex-1">
+            <FontAwesomeIcon icon={faLinkedin} className="h-4 w-4" />
+            <span>linkedin.boseriko.com</span>
+          </div>
+        </div>
       </div>
 
       <hr />
@@ -17,13 +35,13 @@ export default function Resume() {
       {/* Objective */}
       <div className="my-5">
         <h4 className="text-2xl font-bold mb-2">🏁 Objective</h4>
-        <div className="text-justify">
+        <p className="text-justify">
           I am seeking employment with a company where I can use my skills and
           also grow as a person. I want to work in an environment where I can
           learn more knowledge related to my skill set. I want to excel and be
           the best that I can be at programming and also be crucial to any team
           that I can be a part of.
-        </div>
+        </p>
       </div>
 
       <hr />
