fbeac979e0751eadc42e6afb357ffa4c970612db
BosEriko <bos@eriko.ph>
2026-02-03 18:32:52 +0800

    Feature: Add Padding inside Resume PDF

--- components/templates/Resume/index.tsx
+++ components/templates/Resume/index.tsx
@@ -23,23 +23,38 @@ const Resume: React.FC<ResumeProps> = ({ children }) => {
     });
 
     const pdf = new jsPDF("p", "mm", "a4");
-    const pdfWidth = pdf.internal.pageSize.getWidth();
+    const margin = 10;
+
+    const pdfWidth = pdf.internal.pageSize.getWidth() - margin * 2;
     const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
 
-    pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+    const pageHeight = pdf.internal.pageSize.getHeight() - margin * 2;
+    let remainingHeight = pdfHeight;
+    let position = margin;
+
+    pdf.addImage(imgData, "PNG", margin, position, pdfWidth, pdfHeight);
+    remainingHeight -= pageHeight;
+
+    while (remainingHeight > 0) {
+      pdf.addPage();
+      position = -(pdfHeight - pageHeight);
+      pdf.addImage(imgData, "PNG", margin, position, pdfWidth, pdfHeight);
+      remainingHeight -= pageHeight;
+    }
+
     pdf.save("Bos Eriko's Resume.pdf");
   };
 
   return (
     <div className="min-h-screen min-w-screen bg-white">
-      <div className="container mx-auto py-5">
+      <div className="w-200 mx-auto py-5">
         <div>{children}</div>
         <div className="hidden-from-pdf h-15"></div>
         <div className="hidden-from-pdf fixed bottom-0 left-0 w-full bg-blue-950">
           <div className="container mx-auto items-center justify-center gap-5 flex text-white h-15">
             <div>Bos Eriko Reyes’ Resume — ready to download.</div>
             <button
-              className="px-3 py-1 bg-blue-600 hover:bg-blue-700 rounded"
+              className="px-3 py-1 bg-blue-600 hover:bg-blue-700 rounded cursor-pointer"
               onClick={handleDownloadPDF}
             >
               Download
