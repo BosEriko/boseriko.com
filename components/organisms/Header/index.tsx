06c1ab0f435175ce61de098ca42f2231db04cd0e
BosEriko <bos@eriko.ph>
2026-02-03 11:22:37 +0800

    Feature: Add Link to Homepage

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -1,5 +1,23 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 const Header = () => {
-  return <header>header</header>;
+  const router = useRouter();
+  return (
+    <header>
+      <ul>
+        <li>
+          <button
+            className="text-xl font-bold cursor-pointer"
+            onClick={() => router.push("/")}
+          >
+            Home
+          </button>
+        </li>
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
