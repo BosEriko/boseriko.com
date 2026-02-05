8f3b293c0c119e1cea2a0756b535ef5f3451e10a
BosEriko <bos@eriko.ph>
2026-02-05 11:10:24 +0800

    Feature: Add Center Value on Default Template

--- app/page.tsx
+++ app/page.tsx
@@ -17,22 +17,16 @@ export default function Home() {
   }, []);
 
   return (
-    <Template.Default>
-      {/* Page Content */}
-      <div className="p-6 max-w-4xl mx-auto space-y-8 mt-6">
-        {/* Topic Pills */}
-        <div className="hidden-from-pdf flex flex-wrap gap-2 justify-center">
-          {Object.entries(topics).map(([topic, count]) => (
-            <button
-              key={topic}
-              onClick={() => router.push(`/topic/${topic}`)}
-              className="rounded-full bg-zinc-800 hover:bg-zinc-700 px-3 py-1 text-xs font-medium text-white transition"
-            >
-              {topic} {count}
-            </button>
-          ))}
-        </div>
-      </div>
+    <Template.Default center={true}>
+      {Object.entries(topics).map(([topic, count]) => (
+        <button
+          key={topic}
+          onClick={() => router.push(`/topic/${topic}`)}
+          className="rounded-full bg-zinc-800 hover:bg-zinc-700 px-3 py-1 text-xs font-medium text-white transition"
+        >
+          {topic} {count}
+        </button>
+      ))}
     </Template.Default>
   );
 }
--- components/templates/Default/index.tsx
+++ components/templates/Default/index.tsx
@@ -1,24 +1,33 @@
 import React, { ReactNode } from "react";
+import Atom from "@atom";
 import Organism from "@organism";
 import { Roboto_Condensed } from "next/font/google";
 
 interface DefaultProps {
   children: ReactNode;
+  center?: boolean;
 }
 
 const roboto_condensed = Roboto_Condensed({
   weight: ["100"],
 });
 
-const Default: React.FC<DefaultProps> = ({ children }) => {
+const Default: React.FC<DefaultProps> = ({ children, center = false }) => {
   return (
     <div
       className={`${roboto_condensed.className} flex flex-col min-h-screen bg-[#f8f1e3]`}
     >
       <Organism.Header />
-      <div className="flex-1">
-        <div className="container mx-auto px-4 py-10">{children}</div>
-      </div>
+      <Atom.Visibility state={center}>
+        <div className="flex-1 flex items-center justify-center">
+          {children}
+        </div>
+      </Atom.Visibility>
+      <Atom.Visibility state={!center}>
+        <div className="flex-1">
+          <div className="container mx-auto px-4 py-10">{children}</div>
+        </div>
+      </Atom.Visibility>
       <Organism.Footer />
     </div>
   );
