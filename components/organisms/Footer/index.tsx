c57f2338a099b38db881ba094b77cf033ffc0fb2
BosEriko <bos@eriko.ph>
2026-02-05 08:41:18 +0800

    Feature: Add Footer Design

--- components/organisms/Footer/index.tsx
+++ components/organisms/Footer/index.tsx
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return <footer>footer</footer>;
+  return (
+    <footer className="p-4 border-t border-gray-200">
+      <div className="text-center">
+        Bos Eriko Reyes &copy; {new Date().getFullYear()}
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
