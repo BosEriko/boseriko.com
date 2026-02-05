8ffd690b23c540425dfb54f638c34bbde2de94dd
BosEriko <bos@eriko.ph>
2026-02-05 11:15:38 +0800

    Feature: Create a simple homepage

--- app/page.tsx
+++ app/page.tsx
@@ -18,15 +18,26 @@ export default function Home() {
 
   return (
     <Template.Default center={true}>
-      {Object.entries(topics).map(([topic, count]) => (
-        <button
-          key={topic}
-          onClick={() => router.push(`/topic/${topic}`)}
-          className="rounded-full bg-zinc-800 hover:bg-zinc-700 px-3 py-1 text-xs font-medium text-white transition"
-        >
-          {topic} {count}
-        </button>
-      ))}
+      <div className="container mx-auto px-4 flex flex-col gap-5">
+        <h1 className="font-bold text-9xl">Bos Eriko</h1>
+        <hr className="bg-[#f7b43d] w-24 h-2 border-0" />
+        <div>I'm a full stack developer making random things</div>
+        <div className="flex flex-row gap-3">
+          {Object.entries(topics).map(([topic, count]) => (
+            <button
+              key={topic}
+              onClick={() => router.push(`/topic/${topic}`)}
+              className="rounded-full bg-zinc-800 hover:bg-zinc-700 px-3 py-1 text-xs font-medium text-white transition uppercase"
+            >
+              {topic} {count}
+            </button>
+          ))}
+        </div>
+        <div>
+          <button>View Products</button>
+          <button>Resume</button>
+        </div>
+      </div>
     </Template.Default>
   );
 }
