75f6b34053887f93028a9e59bdac57e4675bdcf3
BosEriko <bos@eriko.ph>
2026-02-25 14:55:35 +0800

    Hotfix: Remove key on types

--- components/atoms/Card/index.tsx
+++ components/atoms/Card/index.tsx
@@ -6,7 +6,6 @@ interface ICardProps {
   url: string;
   coverPhotoUrl: string;
   fallbackCoverPhotoUrl?: string | null;
-  key: string,
   children: ReactNode;
 }
 
@@ -14,7 +13,6 @@ const Card: React.FunctionComponent<ICardProps> = ({
   url,
   coverPhotoUrl,
   fallbackCoverPhotoUrl = null,
-  key,
   children,
 }) => {
   const [coverUrl, setCoverUrl] = useState(fallbackCoverPhotoUrl);
